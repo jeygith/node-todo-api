8760c999805e73a94a6379482672c2ad65d0938a
Jeff Githiri <geoffrey.githiri@andela.com>
2019-08-01 09:59:21 +0300

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,45 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+
+var user = {name: 'jeff', age: 26};
+
+var {name} = user;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+    // deleteMany
+    db.collection('Todos').deleteMany({
+        text: 'Eat lunch'
+    }).then((result) => {
+        console.log(result);
+    });
+    // deleteOne
+    db.collection('Todos').deleteOne({
+        text: 'Eat lunch'
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // findOneAndDelete
+
+    db.collection('Todos').findOneAndDelete({
+        completed: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    db.collection('Users').deleteMany({
+        name: 'Jeff'
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
