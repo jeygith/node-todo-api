34fc6b4ccbaac6fd6597237c1b6b3eabe64669cf
Jeff Githiri <geoffrey.githiri@andela.com>
2019-08-02 11:20:16 +0300

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -64,7 +64,29 @@ app.get('/todos/:id', (req, res) => {
         console.log(err);
         res.status(400).send();
     });
-})
+});
+
+/*delete route*/
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+
+    if (!ObjectID.isValid(id)) {
+        console.log('ID is not valid');
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            console.log("no todo");
+            return res.status(404).send({});
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
