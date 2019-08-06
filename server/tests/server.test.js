59fde8a6aae74ee993843fd565602177ac160dbe
Jeff Githiri <geoffrey.githiri@andela.com>
2019-08-06 16:24:00 +0300

    add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -310,4 +310,26 @@ describe('POST /users/login', () => {
                 }).catch((e) => done(e));
             });
     });
-})
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', () => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    if (!user) {
+
+                    }
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e))
+            })
+    })
+});
