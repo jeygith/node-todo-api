737d2a1471dea7eb57dee2df6839b6ef2d3f4f0d
Jeff Githiri <geoffrey.githiri@andela.com>
2019-08-07 15:34:49 +0300

    modify todos to include creator flag

--- server/server.js
+++ server/server.js
@@ -19,11 +19,12 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     console.log(req.body);
 
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -37,8 +38,10 @@ app.post('/todos', (req, res) => {
 });
 
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos',authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.send({
             todos
         })
